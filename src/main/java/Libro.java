85e73989998356b84a992b94c92c789a6ec5ee71
gioele <gioelefriggia@hotmail.com>
2024-03-15 10:01:32 +0000

    Libro

--- src/main/java/Libro.java
+++ src/main/java/Libro.java
@@ -1,2 +1,73 @@
-public class Libro {
+import java.io.Serializable;
+
+public class Libro implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String isbn;
+    private String titolo;
+    private int annoPubblicazione;
+    private int numeroPagine;
+    private String autore;
+    private String genere;
+
+    // Costruttore, getter e setter
+    public Libro() {
+    }
+
+    public Libro(String isbn, String titolo, int annoPubblicazione, int numeroPagine, String autore, String genere) {
+        this.isbn = isbn;
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.numeroPagine = numeroPagine;
+        this.autore = autore;
+        this.genere = genere;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(int annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    public void setNumeroPagine(int numeroPagine) {
+        this.numeroPagine = numeroPagine;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
 }
