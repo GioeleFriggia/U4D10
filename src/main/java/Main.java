4a6cc2f6431ab810ecc247edcda451e698fba4f7
gioele <gioelefriggia@hotmail.com>
2024-03-15 10:02:16 +0000

    Codice per Libro

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        CatalogoBibliotecario catalogo = new CatalogoBibliotecario();
+
+        // Esempio di utilizzo
+        Libro libro1 = new Libro("978-0-13-468599-1", "Clean Code", 2008, 464, "Robert C. Martin", "Programming");
+        catalogo.aggiungiLibro(libro1);
+
+        // Salvataggio su disco
+        catalogo.salvaCatalogo("catalogo.txt");
+
+        // Caricamento dal disco
+        catalogo.caricaCatalogo("catalogo.txt");
+    }
 }
